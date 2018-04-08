4261e846f8fd12b50090b40b193bef24f33a1907
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-08 12:56:30 +0300

    Update createtable1.php

--- createtable1.php
+++ createtable1.php
@@ -2,14 +2,17 @@
 $dsn = ""; 
 $login = ""; 
 $pass = ""; 
-try { 
+
 try {
     $conn = new PDO("sqlsrv:server = tcp:asus19.database.windows.net,1433; Database = dengi", "ram", "Rosbank20");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "CREATE TABLE ИмяТвоейТаблицы( 
+$sql = "CREATE TABLE registration_tab( 
 id INT NOT NULL IDENTITY(1,1), 
 PRIMARY KEY(id), 
-name VARCHAR(50))"; /* описываеш свои переменные и какой тип у них */ 
+name VARCHAR(30), 
+email VARCHAR(30), 
+country VARCHAR(30), 
+famil VARCHAR(30),  
 $conn->query($sql); 
 
 echo '<div style = "color: blue; text-align: center;">Таблица создана!</div><hr>'; 
