be6053481e99d87b5d03500cc6a5ee85fb9e9351
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-08 17:01:05 +0300

    Update carta.php

--- carta.php
+++ carta.php
@@ -1,23 +1,111 @@
-<?php 
-try { 
- $conn = new PDO("sqlsrv:server = tcp:asus19.database.windows.net,1433; Database = dengi", "ram", "Rosbank20");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<html>
+<head>
+<Title>Перевод денежных средств</Title>
+<style type="text/css">
+body { background-color:
+#fff; border-top: solid 10px #000;
+color: #333; font-size: .85em;
+margin: 20; padding: 20;
+font-family: "Segoe UI",
+Verdana, Helvetica, Sans-Serif;
+}
+h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+h1 { font-size: 2em; }
+h2 { font-size: 1.75em; }
+h3 { font-size: 1.2em; }
+table { margin-top: 0.75em; }
+th { font-size: 1.2em;
+text-align: left; border: none; padding-left: 0; }
+td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Перевод денежных средств</h1>
+<p>Fill in your name and
+email address, then click <strong>Submit</strong>
+to register.</p>
+<form method="post" action="index.php" enctype="multipart/form-data" >
+<input type ="text" name ="name" id ="name" placeholder ="Введите данные какрты">
+<input type ="text" name ="email" id ="email" placeholder ="Ведите CVC..">
+<select name="country">
+<option value="">All</option>
+<option value="Russia">01</option>
+<option value="USA">02</option>
+<option value="Germany">03</option>
+<option value="Japan">04</option>
+<option value="China">05</option>
+ <option value="China">06</option>
+ <option value="China">07</option>
+</select>
+<input type ="submit" name ="submit" value ="Отправить">
+<input type="submit" name="filter" value="Фильтр">
+</form>
 
-
-for($i = 4;$i<10;$i++) 
-{ 
-$sql_in = 
-"INSERT INTO Card (Ncard, Balance,Phone) 
-VALUES (?,?,?)"; 
-$stmt = $conn->prepare($sql_in); 
-$stmt->bindValue(1, "427612345678910".$i); 
-$stmt->bindValue(2, 10000); 
-$stmt->bindValue(3, "8999999999".$i); 
-$stmt->execute(); 
-} 
-} 
-catch (PDOException $e) { 
-print("Error connecting to SQL Server."); 
-die(print_r($e)); 
-} 
+<?php
+$dsn = "sqlsrv:server = tcp:asus19.database.windows.net,1433; Database = dengi";
+$username = "ram";
+$password = "Rosbank20";
+try {
+$conn = new PDO($dsn, $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+print("Ошибка подключения к SQL Server.");
+die(print_r($e));
+}
+if(!empty($_POST)) {
+try {
+$name = $_POST['name'];
+$email = $_POST['email'];
+$date = date("Y-m-d");
+$country = $_POST['country'];
+if ($name == "" || $email == "" || $country == "") {
+echo "<h3>Не заполнены поля name и famil.</h3>";
+}
+else {
+$sql_insert ="INSERT INTO registration_on (name, email, date, country) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($sql_insert);
+$stmt->bindValue(1, $name);
+$stmt->bindValue(2, $email);
+$stmt->bindValue(3, $date);
+$stmt->bindValue(4, $country);
+$stmt->execute();
+echo "<h3>Вы зарегистрировались!</h3>";
+}
+}
+catch(Exception $e) {
+die(var_dump($e));
+}
+}
+$sql_select = "SELECT * FROM registration_on";
+$stmt = $conn->query($sql_select);
+$stmt->execute();
+if(isset($_POST['filter'])) {
+$gender = $_POST['country'];
+$sql_select = "SELECT * FROM registration_on WHERE country like :country";
+$stmt = $conn->prepare($sql_select);
+$stmt->execute(array(':country'=>$country.'%'));
+}
+$registrants = $stmt->fetchAll();
+if(count($registrants) > 0) {
+echo "<h2>Люди, которые оставили заявки на перевод денег.:</h2>";
+echo "<table>";
+echo "<tr><th>Name</th>";
+echo "<th>famil</th>";
+echo "<th>Country</th>";
+echo "<th>Date</th></tr>";
+foreach($registrants as $registrant) {
+echo "<td>".$registrant['name']."</td>";
+echo "<td>".$registrant['email']."</td>";
+echo "<td>".$registrant['country']."</td>";
+echo "<td>".$registrant['date']."</td></tr>";
+}
+echo "</table>";
+}
+else {
+echo "<h3>В настоящее время никто не оставил заявку.</h3>";
+}
 ?>
+
