e0f05971fd3ae8d75b921de789b2a2d938f66007
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-08 10:52:54 +0300

    Create den.php

---
+++ den.php
@@ -0,0 +1,15 @@
+ <?
+if($curret=file_get_contents('money')){$curret=(integer)$curret;}else{$curret=0;}
+if(isset($_POST['money']) and (integer)$_POST['money'])
+{
+    if($file=fopen('money','w'))
+    {
+        fwrite($file,$curret+(integer)$_POST['money']);
+        fclose($file);
+    }
+}
+?>
+<form action="" method="post">
+    <input type="text" name="money" /> <input type="submit" value="добавить" />
+</form>
+<?if($curret){?><div>Ваш баланс: <?echo $curret;?></div><?}?> 
