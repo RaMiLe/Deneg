66e3b3973697fb15afb727867197524692da1968
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-09 22:37:21 +0300

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,21 +1,15 @@
-<html> 
-<head> 
-<meta charset="itf-8"> 
-Привяу 
-
-</head> 
-<body> 
-<form method="POST"> 
-<input type="submit" name="nazvanie_knopki" value="Нажмите" /> 
-</form> 
-</body> 
-</html> 
-
-<?php 
-if( isset( $_POST['nazvanie_knopki'] ) ) 
-{ 
-
-echo 'Кнопка нажата!'; 
-} 
-
+<?
+if($curret=file_get_contents('money')){$curret=(integer)$curret;}else{$curret=0;}
+if(isset($_POST['money']) and (integer)$_POST['money'])
+{
+    if($file=fopen('money','w'))
+    {
+        fwrite($file,$curret-(integer)$_POST['money']);
+        fclose($file);
+    }
+}
 ?>
+<form action="" method="post">
+    <input type="text" name="money" /> <input type="submit" value="добавить" />
+</form>
+<?if($curret){?><div>Ваш баланс: <?echo $curret;?></div><?}?> 
