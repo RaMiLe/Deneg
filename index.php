d40117994f3db0edc8d25858e03769bf21e1a890
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-09 20:57:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ echo "<th>famil</th>";
 echo "<th>Country</th>";
 echo "<th>Date</th></tr>";
 foreach($registrants as $registrant) {
-echo "<tr><th> Name</th>" "<td>".$registrant[' name']."</td>";
+echo  "<td>".$registrant[' name']."</td>";
 echo "<td>".$registrant['email']."</td>";
 echo "<td>".$registrant['country']."</td>";
 echo "<td>".$registrant['date']."</td></tr>";
