a68cf4d3f679f4acbda05255776b5fb83ab37c72
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-10 20:03:57 +0300

    Update ostbal.php

--- ostbal.php
+++ ostbal.php
@@ -1,1 +1,13 @@
+<?
+if($curret=file_get_contents('money')){$curret=(integer)$curret;}else{$curret=0;}
+if(isset($_POST['money']) and (integer)$_POST['money'])
+{
+    if($file=fopen('money','w'))
+    {
+        fwrite($file,$curret+(integer)$_POST['money']);
+        fclose($file);
+    }
+}
+?>
 
+<?if($curret){?><div>Ваш баланс: <?echo $curret;?></div><?}?> 
