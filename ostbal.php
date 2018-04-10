ce7b196069a6f82e43f055ccbc8f5815c4add98d
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-10 20:06:43 +0300

    Update ostbal.php

--- ostbal.php
+++ ostbal.php
@@ -7,8 +7,9 @@ if(isset($_POST['money']) and (integer)$_POST['money'])
         fwrite($file,$curret+(integer)$_POST['money']);
         fclose($file);
     }
-    echo "<h3>Таблица создана.</h3>"
+  
 }
 ?>
 
 <?if($curret){?><div>Ваш остаток баланса: <?echo $curret;?></div><?}?> 
+  echo "<h3>Таблица создана.</h3>"
