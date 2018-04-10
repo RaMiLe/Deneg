201dc2ba53935440f2673a12c19a98a17d3943bc
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2018-04-10 19:48:30 +0300

    Update den.php

--- den.php
+++ den.php
@@ -11,5 +11,15 @@ if(isset($_POST['money']) and (integer)$_POST['money'])
 ?>
 <form action="" method="post">
     <input type="text" name="money" /> <input type="submit" value="добавить" />
+  <input type="button" value="Далее" name="buttonreg" onClick="but1()" />
+		       
+	<script>
+function but1()
+{
+     window.location = "den.php"
+}
+</script>		
+</form>
+
 </form>
 <?if($curret){?><div>Ваш баланс: <?echo $curret;?></div><?}?> 
